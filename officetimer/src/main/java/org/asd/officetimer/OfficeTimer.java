0588c4a93f790bb6282d6683e2561db7151c2b44
AbhayD <abhay.dandekar@xoriant.com>
2017-02-01 17:54:37 +0530

    Added time to leave

--- officetimer/src/main/java/org/asd/officetimer/OfficeTimer.java
+++ officetimer/src/main/java/org/asd/officetimer/OfficeTimer.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.Callable;
 
 /**
@@ -22,12 +23,17 @@ import java.util.concurrent.Callable;
 public class OfficeTimer implements Callable<Integer> {
 	
 	final private Instant start;
+	final private long mandatoryOfficeHours = 7;
+	final private long mandatoryOfficeMins = 30;
 	final private String msg = "Your today's office in-time : ";
+	final private String msg2 = "Your office check-in time is : ";
+	final private String msg3 = "You can leave from office at : ";
 	final private StringBuilder buffer = new StringBuilder();
 		
 	public OfficeTimer( ) {
 		this.start = Instant.now();
-		System.out.println("Your office check-in time is : " + LocalDateTime.ofInstant(start, ZoneId.systemDefault()));
+		System.out.println(msg2 + LocalDateTime.ofInstant(start, ZoneId.systemDefault()));
+		System.out.println(msg3 + LocalDateTime.ofInstant(start.plus(mandatoryOfficeHours, ChronoUnit.HOURS).plus(mandatoryOfficeMins, ChronoUnit.MINUTES), ZoneId.systemDefault()));
 	}
 	
 	public void displayOfficeInTime(){
@@ -37,7 +43,7 @@ public class OfficeTimer implements Callable<Integer> {
 		buffer.append(msg);
 		buffer.append(diff.toHours());
 		buffer.append("hours ");
-		buffer.append(diff.toMinutes());
+		buffer.append(diff.toMinutes() % 60);
 		buffer.append("mins ");
 		buffer.append(diff.getSeconds() % 60 );
 		buffer.append("secs");
